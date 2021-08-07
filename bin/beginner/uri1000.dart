626263767641f39450762b135ac3a95ec4a553de
Luciano <luciano01.dev@gmail.com>
2021-08-06 23:33:59 -0300

    First commit

---
+++ bin/beginner/uri1000.dart
@@ -0,0 +1,6 @@
+/* Your first program in any programming language is usually "Hello World!". 
+In this first problem all you have to do is print this message on the screen. */
+
+void main() {
+  print('Hello World!');
+}
